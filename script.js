efe86fc7e2d9656cad8494a606ac8e81b75b47e8
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-07-06 00:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// script.js
+window.addEventListener('DOMContentLoaded', () => {
+  const element = document.getElementById('level');
+  let level = 0;
+
+  while (element.parentNode) {
+    element = element.parentNode;
+    level++;
+  }
+
+  alert(`The level of the element is: ${level}`);
+});
